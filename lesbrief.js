14674e798d76205ec2a133dc656e880cb376cdc5
Ryan L <31843665+ryanlamsberg@users.noreply.github.com>
2019-05-15 08:59:31 +0200

    Lesbrief 2 odpracht 1

---
+++ lesbrief.js
@@ -0,0 +1,14 @@
+const TeleBot = require('telebot');
+
+const bot = new TeleBot({
+    token: '790644618:AAE1UPR_wv20eHj3_PrH9fr-4kRZqpZVaW8' // Telegram Bot API token.
+});
+
+bot.on('/start', function (msg) {
+  return bot.sendMessage(msg.from.id, "Hello world!");
+});
+bot.on('/wiebenje', function (msg) {
+  return bot.sendMessage(msg.from.id, "Ik ben RyxnBot, de bot die Ryan gebruikt om zijn badge te halen!");
+});
+
+bot.start();
