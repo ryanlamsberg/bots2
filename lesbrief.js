32be3118a1e29301b0bd56a09d5a30c7ec6ba0d2
Ryan L <31843665+ryanlamsberg@users.noreply.github.com>
2019-05-16 09:18:37 +0200

    Reply command toegevoegd

--- lesbrief.js
+++ lesbrief.js
@@ -4,13 +4,18 @@ const bot = new TeleBot({
     token: '790644618:AAE1UPR_wv20eHj3_PrH9fr-4kRZqpZVaW8' // Telegram Bot API token.
 });
 
-bot.on('/start', function (msg) {
-  return bot.sendMessage(msg.from.id, "Hello world!");
+bot.on('/hoi', (msg) => {
+    var arg = { replyToMessage: msg.message_id }
+    return bot.sendMessage(msg.from.id, "Goeiedag makker!", arg);
 });
 bot.on('/wiebenje', function (msg) {
   return bot.sendMessage(msg.from.id, "Ik ben RyxnBot, de bot die Ryan gebruikt om zijn badge te halen!");
 });
 bot.on('/meme', (msg) => {
     return bot.sendPhoto(msg.from.id, "images/meme.png");
+});
+bot.on('/versie', (msg) => {
+    var arg = { replyToMessage: msg.message_id }
+    return bot.sendMessage(msg.from.id, "Dit is versie 3.5", arg);
 });
 bot.start();
