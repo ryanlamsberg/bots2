5c71e5b5156308bdcecb325c60a5d90ace249940
Ryan L <31843665+ryanlamsberg@users.noreply.github.com>
2019-05-28 15:12:53 +0200

    Fotos

--- lesbrief.js
+++ lesbrief.js
@@ -4,18 +4,18 @@ const bot = new TeleBot({
     token: '790644618:AAE1UPR_wv20eHj3_PrH9fr-4kRZqpZVaW8' // Telegram Bot API token.
 });
 
-bot.on('/hoi', (msg) => {
-    var arg = { replyToMessage: msg.message_id }
-    return bot.sendMessage(msg.from.id, "Goeiedag makker!", arg);
+bot.on('/start', function (msg) {
+  return bot.sendMessage(msg.from.id, "Hallo, ik ga u helpen door u te informeren over onze school!");
 });
-bot.on('/wiebenje', function (msg) {
-  return bot.sendMessage(msg.from.id, "Ik ben RyxnBot, de bot die Ryan gebruikt om zijn badge te halen!");
-});
-bot.on('/meme', (msg) => {
-    return bot.sendPhoto(msg.from.id, "images/meme.png");
+bot.on('/plattegrond 1', (msg) => {
+    return bot.sendPhoto(msg.from.id, "images/beganerond.png");
 });
 bot.on('/versie', (msg) => {
     var arg = { replyToMessage: msg.message_id }
     return bot.sendMessage(msg.from.id, "Dit is versie 3.5", arg);
+});
+bot.on('/help', (msg) => {
+    var arg = { replyToMessage: msg.message_id }
+    return bot.sendMessage(msg.from.id, "hoi /wiebenje , /meme , /versie", arg);
 });
 bot.start();
