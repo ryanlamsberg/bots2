6a08ae7623b4fd53063ecaa9ea988e5a6e3d5318
Ryan L <31843665+ryanlamsberg@users.noreply.github.com>
2019-05-28 15:19:57 +0200

    fotos 2

--- lesbrief.js
+++ lesbrief.js
@@ -8,7 +8,7 @@ bot.on('/start', function (msg) {
   return bot.sendMessage(msg.from.id, "Hallo, ik ga u helpen door u te informeren over onze school!");
 });
 bot.on('/plattegrond 1', (msg) => {
-    return bot.sendPhoto(msg.from.id, "images/beganerond.png");
+    return bot.sendPhoto(msg.from.id, "images/beganegrond.png");
 });
 bot.on('/versie', (msg) => {
     var arg = { replyToMessage: msg.message_id }
